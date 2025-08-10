43d9c60966c4c206e0822703a473c4a1fcc2bcba
Inhye Kang <inhyek91@gmail.com>
2025-08-10 22:02:56 +0900

    fix: vue-admin/main.js 코드 정리

--- myFluffy-web/admin/vue-admin/src/main.js
+++ myFluffy-web/admin/vue-admin/src/main.js
@@ -3,29 +3,29 @@ import App from './App.vue';
 import { createPinia } from 'pinia';
 import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'vue-toastification/dist/index.css';
 import Toast from 'vue-toastification';
 // Font Awesome 관련 설정
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faHome, faUser, faBell, faPlus, faUsersCog, faThList, faFileAlt } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { Ckeditor } from '@ckeditor/ckeditor5-vue';
 import router from './router';
 
 // Font Awesome 아이콘을 라이브러리에 추가
 library.add(faHome, faUser, faBell, faPlus, faUsersCog, faThList, faFileAlt)
 
-const app = createApp(App)
-app.use(createPinia())
-app.use(router)
-const toastOptions = {
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(Toast, {
   position: 'top-right',
   timeout: 3000,
   closeOnClick: true,
   pauseOnHover: true
-};
-app.use(Toast, toastOptions);
-app.component('ckeditor', Ckeditor)
-app.component('font-awesome-icon', FontAwesomeIcon)
-app.mount('#app')
+});
+app.component('ckeditor', Ckeditor);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.mount('#app');
